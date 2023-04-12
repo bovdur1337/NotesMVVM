37018e6e1a36dc3198a9abce2ba3e2e107aaa456
Olbabej <0lbabej@gmail.com>
2023-04-12 13:07:08 +0200

    activities

--- app/src/main/java/com/example/notesappmvvm/activities/AddEditActivity.kt
+++ app/src/main/java/com/example/notesappmvvm/activities/AddEditActivity.kt
@@ -1,12 +1,85 @@
 package com.example.notesappmvvm.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.example.notesappmvvm.R
+import com.example.notesappmvvm.db.Note
+import com.example.notesappmvvm.model.NoteViewModel
 
 class AddEditActivity : AppCompatActivity() {
+
+    lateinit var noteTitleET: EditText
+    lateinit var noteDesctiptionET: EditText
+    lateinit var applyBTN: Button
+    lateinit var viewModel: NoteViewModel
+    var noteId = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_edit)
+
+        noteTitleET = findViewById(R.id.ETnoteTitle)
+        noteDesctiptionET = findViewById(R.id.ETnoteDescription)
+        applyBTN = findViewById(R.id.BTNaddUpdate)
+        val noteType = intent.getStringExtra("noteType")
+
+        viewModel = ViewModelProvider(this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application))
+            .get(NoteViewModel::class.java)
+
+        fun isEditIntentCheck(){
+            if (noteType.equals("Edit")){
+                val noteTitle = intent.getStringExtra("noteTitle")
+                val noteDescription = intent.getStringExtra("noteDescription")
+                noteId = intent.getIntExtra("noteId", 0)
+
+                noteTitleET.setText(noteTitle)
+                noteDesctiptionET.setText(noteDescription)
+            }
+        }
+
+        isEditIntentCheck()
+
+        fun upsertData() {
+            if (noteType.equals("Edit")) { updateData() } else { insertData() }
+        }
+
+        applyBTN.setOnClickListener{ upsertData() }
+    }
+
+    private fun insertData(){
+        val noteTitle = noteTitleET.text.toString()
+        val noteDescription = noteDesctiptionET.text.toString()
+
+        if (noteTitle.isNotEmpty() && noteDescription.isNotEmpty()){
+            viewModel.insert(Note(noteTitle, noteDescription))
+            Toast.makeText(this, "Note Added", Toast.LENGTH_SHORT).show()
+
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            this.finish()
+        }else{
+            Toast.makeText(this, "Enter Data", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateData(){
+        val noteTitle = noteTitleET.text.toString()
+        val noteDescription = noteDesctiptionET.text.toString()
+
+        if (noteTitle.isNotEmpty() && noteDescription.isNotEmpty()){
+            val updatedNote = Note(noteTitle, noteDescription, noteId)
+            viewModel.update(updatedNote)
+            Toast.makeText(this, "Note Updated", Toast.LENGTH_SHORT).show()
+
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            this.finish()
+        }else{
+            Toast.makeText(this, "Enter Data", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/notesappmvvm/activities/MainActivity.kt
+++ app/src/main/java/com/example/notesappmvvm/activities/MainActivity.kt
@@ -1,12 +1,67 @@
 package com.example.notesappmvvm.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.notesappmvvm.R
+import com.example.notesappmvvm.db.Note
+import com.example.notesappmvvm.model.NoteViewModel
+import com.example.notesappmvvm.rv_adapter.NoteClickDeleteInterface
+import com.example.notesappmvvm.rv_adapter.NoteClickInterface
+import com.example.notesappmvvm.rv_adapter.NoteRVadapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NoteClickInterface, NoteClickDeleteInterface {
+
+    lateinit var notesRV: RecyclerView
+    lateinit var addFAB: FloatingActionButton
+    lateinit var viewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        notesRV = findViewById(R.id.RVnotes)
+        addFAB = findViewById(R.id.FABaddNote)
+
+        notesRV.layoutManager = LinearLayoutManager(this)
+        val noteRVadapter = NoteRVadapter(this, this, this)
+        notesRV.adapter = noteRVadapter
+
+        viewModel = ViewModelProvider(this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application))
+            .get(NoteViewModel::class.java)
+        viewModel.allNotes.observe(this, Observer {list ->
+            list?.let {
+                noteRVadapter.updateList(it)
+            }
+        })
+
+        addFAB.setOnClickListener{
+            val intent = Intent(this, AddEditActivity::class.java)
+            startActivity(intent)
+            this.finish()
+        }
+    }
+
+    override fun onNoteClick(note: Note) {
+        val intent = Intent(this, AddEditActivity::class.java)
+        intent.putExtra("noteType", "Edit")
+        intent.putExtra("noteTitle", note.noteTitle)
+        intent.putExtra("noteDescription", note.noteDescription)
+        intent.putExtra("noteId", note.id)
+        startActivity(intent)
+        this.finish()
+    }
+
+    override fun onDeleteClick(note: Note) {
+        viewModel.delete(note)
+        Toast.makeText(this, "${note.noteTitle} deleted", Toast.LENGTH_SHORT).show()
     }
 }
